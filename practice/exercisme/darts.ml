d7fbf5d350050ca3e6c0ed14d901ac3be839ef38
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2025-12-09 00:28:23 +0100

    add darts exercise revisted

--- practice/exercisme/darts.ml
+++ practice/exercisme/darts.ml
@@ -1,6 +1,8 @@
 (* equation of a disk (the inner circle) x^2 + y^2 < r  *)
+let distance x y = x *. x  +. y *. y |> sqrt
+
 let score (x: float) (y: float): int =
-  if x ** 2.  +. y ** 2. <= 1. then 10
-  else if x ** 2. +. y ** 2. <= 25. then 5 
-  else if  x ** 2.  +. y ** 2. <= 100. then 1 
+  if distance x y <= 1. then 10
+  else if distance x y <= 5. then 5 
+  else if  distance x y <= 10. then 1 
   else  0
